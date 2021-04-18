0e41e4881a64ffec112bf277b796ca1c95d06e0a
DavidBPowers <55028524+DavidBPowers@users.noreply.github.com>
2021-04-18 19:09:23 -0400

    Update ftpserver.java

--- ftpserver.java
+++ ftpserver.java
@@ -10,143 +10,152 @@ import javax.swing.*;
 import javax.xml.crypto.Data;
 
 public class ftpserver extends Thread {
-        private Socket connectionSocket;
-        int port;
-        int count = 1;
-        int turn = 1;
-        char disc = 'R';
-        char[][] board = new char[6][7];
+    private Socket connectionSocket;
+    int port;
+    int count = 1;
+    int turn = 1;
+    char disc = 'R';
+    char[][] board = new char[6][7];
 
-        public ftpserver(Socket connectionSocket) {
-            this.connectionSocket = connectionSocket;
+    public ftpserver(Socket connectionSocket) {
+        this.connectionSocket = connectionSocket;
+    }
+
+
+    public void run() {
+        if (count == 1)
+            System.out.println("User connected" + connectionSocket.getInetAddress());
+        count++;
+
+        try
+        {
+            processRequest();
+        }
+        catch (Exception e)
+        {
+            System.out.println(e);
         }
 
+    }
 
-        public void run() {
+    private void processRequest() throws Exception {
+        String fromClient;
+        String clientCommand;
+        String clientFileName;
+        byte[] data;
+        String frstln;
+
+        while (true)
+        {
             if (count == 1)
                 System.out.println("User connected" + connectionSocket.getInetAddress());
             count++;
 
-            try {
-                processRequest();
+            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
 
-            } catch (Exception e) {
-                System.out.println(e);
-            }
+            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
 
-        }
+            fromClient = inFromClient.readLine();
 
-        private void processRequest() throws Exception {
-            String fromClient;
-            String clientCommand;
-            String clientFileName;
-            byte[] data;
-            String frstln;
+            System.out.println(fromClient);
+            StringTokenizer tokens = new StringTokenizer(fromClient);
 
-            while (true) {
-                if (count == 1)
-                    System.out.println("User connected" + connectionSocket.getInetAddress());
-                count++;
-
-                DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-
-                BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-
-                fromClient = inFromClient.readLine();
-
-                System.out.println(fromClient);
-                StringTokenizer tokens = new StringTokenizer(fromClient);
-
-                frstln = tokens.nextToken();
-                port = Integer.parseInt(frstln);
-                clientCommand = tokens.nextToken();
-                System.out.println(clientCommand);
+            frstln = tokens.nextToken();
+            port = Integer.parseInt(frstln);
+            clientCommand = tokens.nextToken();
+            System.out.println(clientCommand);
 
 
-                if (clientCommand.equals("LIST:")) {
-                    String curDir = System.getProperty("user.dir");
+            if (clientCommand.equals("LIST:"))
+            {
+                String curDir = System.getProperty("user.dir");
 
-                    Socket dataSocket = new Socket(connectionSocket.getInetAddress(), port);
-                    DataOutputStream dataOutToClient =
-                            new DataOutputStream(dataSocket.getOutputStream());
-                    File dir = new File(curDir);
+                Socket dataSocket = new Socket(connectionSocket.getInetAddress(), port);
+                DataOutputStream dataOutToClient =
+                        new DataOutputStream(dataSocket.getOutputStream());
+                File dir = new File(curDir);
 
-                    String[] children = dir.list();
-                    if (children == null) {
-                        // Either dir does not exist or is not a directoryRET
-                    } else {
-                        for (int i = 0; i < children.length; i++) {
-                            // Get filename of file or directory
-                            String filename = children[i];
+                String[] children = dir.list();
+                if (children == null) {
+                    // Either dir does not exist or is not a directoryRET
+                }
+                else
+                {
+                    for (int i = 0; i < children.length; i++) {
+                        // Get filename of file or directory
+                        String filename = children[i];
 
-                            if (filename.endsWith(".txt"))
-                                dataOutToClient.writeUTF(children[i]);
-                            //System.out.println(filename);
-                            if (i - 1 == children.length - 2) {
-                                dataOutToClient.writeUTF("eof");
-                                // System.out.println("eof");
-                            }//if(i-1)
-
-
-                        }//for
-
-                        dataSocket.close();
-                        //System.out.println("Data Socket closed");
-                    }//else
+                        if (filename.endsWith(".txt"))
+                            dataOutToClient.writeUTF(children[i]);
+                        //System.out.println(filename);
+                        if (i - 1 == children.length - 2) {
+                            dataOutToClient.writeUTF("eof");
+                            // System.out.println("eof");
+                        }//if(i-1)
 
 
-                }//if list:
-
-
-                if (clientCommand.equals("RETR:")) {
-                    clientFileName = tokens.nextToken();
-
-                    String curDir = System.getProperty("user.dir");
-
-                    File file = new File(curDir + "/" + clientFileName);
-		    file.createNewFile();
-
-                    Socket dataSocket = new Socket(connectionSocket.getInetAddress(), port);
-                    DataInputStream fileIn = new DataInputStream(new FileInputStream(file));
-                    DataOutputStream dataOutToClient = new DataOutputStream(dataSocket.getOutputStream());
-
-                    byte[] buffer = new byte[1024];
-                    while ((fileIn.read(buffer))!=-1) {
-                        dataOutToClient.write(buffer, 0, buffer.length);
-                    }
-
-                    System.out.println("\nFile " + clientFileName + " sent");
+                    }//for
 
                     dataSocket.close();
+                    //System.out.println("Data Socket closed");
+                }//else
+
+
+            }//if list:
+
+
+            if (clientCommand.equals("RETR:")) {
+                clientFileName = tokens.nextToken();
+
+                String curDir = System.getProperty("user.dir");
+
+                File file = new File(curDir + "/" + clientFileName);
+                file.createNewFile();
+
+                Socket dataSocket = new Socket(connectionSocket.getInetAddress(), port);
+                DataInputStream fileIn = new DataInputStream(new FileInputStream(file));
+                DataOutputStream dataOutToClient = new DataOutputStream(dataSocket.getOutputStream());
+
+                byte[] buffer = new byte[1024];
+                while ((fileIn.read(buffer))!=-1)
+                {
+                    dataOutToClient.write(buffer, 0, buffer.length);
                 }
 
-                if (clientCommand.equals("STOR:")){
-                    clientFileName = tokens.nextToken();
-		    
-                    String curDir = System.getProperty("user.dir");
+                System.out.println("\nFile " + clientFileName + " sent");
 
-		    File file = new File(curDir + "/" + clientFileName);
+                dataSocket.close();
+            }
 
-                    Socket dataSocket = new Socket(connectionSocket.getInetAddress(), port);
-                    DataInputStream inData = new DataInputStream(new BufferedInputStream(dataSocket.getInputStream()));
-		    byte[] buffer = new byte[1024];                    
-		    DataOutputStream dataOut = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(file)));
+            if (clientCommand.equals("STOR:"))
+            {
+                clientFileName = tokens.nextToken();
 
-		    while ((inData.read(buffer))!=-1){
-			dataOut.write(buffer, 0, buffer.length);
-		    }
-                    
-		    dataOut.close();
-                    System.out.println("\n File " + clientFileName + " saved");
+                String curDir = System.getProperty("user.dir");
 
+                File file = new File(curDir + "/" + clientFileName);
+
+                Socket dataSocket = new Socket(connectionSocket.getInetAddress(), port);
+                DataInputStream inData = new DataInputStream(new BufferedInputStream(dataSocket.getInputStream()));
+                byte[] buffer = new byte[1024];
+                DataOutputStream dataOut = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(file)));
+
+                while ((inData.read(buffer))!=-1)
+                {
+                    dataOut.write(buffer, 0, buffer.length);
                 }
-		
-		if (clientCommand.equals("QUIT")){
-			connectionSocket.close();
-			System.out.println("\nConnection is closed.");	
-			break;
-			
-		}
+
+                dataOut.close();
+                        System.out.println("\n File " + clientFileName + " saved");
+
+            }
+
+            if (clientCommand.equals("QUIT")){
+                connectionSocket.close();
+                System.out.println("\nConnection is closed.");
+                break;
+
+            }
         }
     }
     private void initBoard(){
@@ -221,7 +230,7 @@ public class ftpserver extends Thread {
                             {
                                 continue;
                             }
-                            
+
                             if (board[rowMove + i][colMove + j] == disc)
                             {
                                 return disc;
